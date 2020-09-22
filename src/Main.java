f4a49a3e35610c5347b601e3a3d6163b2ac4aca4
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2020-09-22 16:16:42 +0300

    revision

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        float weight = 53;
-        float height = (float) 1.70;
+        float weight = 53f;
+        float height = 1.70f;
 
         BmiService service = new BmiService();
         float bmi = service.calculate(weight, height);
-        System.out.println(bmi);
+        System.out.printf("Индекс массы тела %.2f", bmi);
     }
 }
